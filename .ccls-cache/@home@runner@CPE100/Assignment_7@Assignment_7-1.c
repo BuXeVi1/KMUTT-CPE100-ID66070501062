ead2e02d3b59282cb576ef67e8a5f87310dc036e
BuXeVi1 <61242682+BuXeVi1@users.noreply.github.com>
2023-10-12 03:55:20 +0000

    update assg7-1

--- .ccls-cache/@home@runner@CPE100/Assignment_7@Assignment_7-1.c
+++ .ccls-cache/@home@runner@CPE100/Assignment_7@Assignment_7-1.c
@@ -1,45 +1,43 @@
 #include <stdio.h>
 
-int findMinimum(int num1, int num2){
-    if (num1 > num2)
-        return num2;
-    else
-        return num1;
+int findMinimum(int num1, int num2) {
+  if (num1 < num2)
+    return num1;
+  else
+    return num2;
 }
 
-int findMaximum(int num1, int num2){
-    if (num1 > num2)
-        return num1;
-    else
-        return num2;
+void findMaximum(int num1, int num2, int *max) {
+  if (num1 > num2)
+    *max = num1;
+  else
+    *max = num2;
 }
 
-int main(){
-    int num1, num2, option = 0, min, max;
-    printf("Enter 2 numbers: ");
-    scanf("%d %d", &num1, &num2);
+int main() {
+  int num1, num2, option = 0, min, max;
+  printf("Enter 2 numbers: ");
+  scanf("%d %d", &num1, &num2);
 
-    while (option != 3)
-    {
-        printf("\nChoose what to do from options below");
-        printf("\n1. Find minimum");
-        printf("\n2. Find maximum");
-        printf("\n3. Exit program");
-        printf("\nEnter option no.: ");
-        scanf("%d", &option);
+  while (option != 3) {
+    printf("\nChoose what to do from options below");
+    printf("\n1. Find minimum");
+    printf("\n2. Find maximum");
+    printf("\n3. Exit program");
+    printf("\nEnter option no.: ");
+    scanf("%d", &option);
 
-        if (option == 1)
-        {
-            min = findMinimum(num1, num2);
-            printf("Minimum value is: %d\n", min);
-        }
-        else if (option == 2)
-        {
-            max = findMaximum(num1, num2);
-            printf("Maximum value is: %d\n", max);
-        }
+    if (option == 1) {
+      min = findMinimum(num1, num2);
+      printf("Minimum value is: %d\n", min);
+    } else if (option == 2) {
+      findMaximum(num1, num2, &max);
+      printf("Maximum value is: %d\n", max);
+    } else if (option == 3) {
+      printf("End of program. Goodbye.\n");
+    } else {
+      printf("Invalid option. Please choose a valid option (1, 2, or 3).\n");
     }
-
-    printf("End of program. Goodbye.\n");
-    return 0;
+  }
+  return 0;
 }
